8f22e5007b337b267b7d9ad176938eb80289b5dc
dacitto <dacidigital@gmail.com>
2022-01-02 13:43:28 +0100

    add firebase config file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      Products 
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
     </div>
   );
 }
